f9ff39c9dd4cb738fcabcc03bff63dd4563508a6
Carlos Eduardo <dev.carlosoliveira@gmail.com>
2025-06-15 11:03:33 -0300

    add imports

--- NotasTelas/src/main/java/Entidades/TelaPrincipal.java
+++ NotasTelas/src/main/java/Entidades/TelaPrincipal.java
@@ -4,6 +4,24 @@
  */
 package Entidades;
 
+import java.io.IOException;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.text.MaskFormatter;
+
 /**
  *
  * @author Carlos
