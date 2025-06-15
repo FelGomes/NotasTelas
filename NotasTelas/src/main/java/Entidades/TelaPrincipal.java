ace03851dbcb615c8aea5ebfe8f635f76564fd72
Carlos Eduardo <dev.carlosoliveira@gmail.com>
2025-06-15 11:15:31 -0300

    adicionando todas os botões que levam a as telas de cada classe, ainda falta as funcionalidades

--- NotasTelas/src/main/java/Entidades/TelaPrincipal.java
+++ NotasTelas/src/main/java/Entidades/TelaPrincipal.java
@@ -28,4 +28,72 @@ import javax.swing.text.MaskFormatter;
  */
 public class TelaPrincipal {
     
+    
+    public static void montarTelaInicial() throws IOException {
+        
+        final JFrame jFrameInicial = new JFrame("Sistema de notas");
+        
+        jFrameInicial.setBounds(200, 170, 850, 600);
+        jFrameInicial.setLayout(null);
+        
+        JLabel labelUsuario = new JLabel("Usuarios");
+        labelUsuario.setBounds(20, 20, 100, 100);
+        labelUsuario.setHorizontalAlignment(JLabel.CENTER);
+        
+        JButton buttonUsuario = new JButton();
+        buttonUsuario.setBounds(150,175, 100, 50);
+        buttonUsuario.add(labelUsuario);
+        jFrameInicial.add(buttonUsuario);
+        
+        JLabel labelAlunos = new JLabel("Alunos");
+        labelAlunos.setBounds(20, 20, 100, 100);
+        labelAlunos.setHorizontalAlignment(JLabel.CENTER);
+        
+        JButton buttonAlunos = new JButton();
+        buttonAlunos.setBounds(275, 175, 100, 50);
+        buttonAlunos.add(labelAlunos);
+        jFrameInicial.add(buttonAlunos);
+        
+        JLabel labelBoletim = new JLabel("Boletim");
+        labelBoletim.setBounds(20, 20, 100, 100);
+        labelBoletim.setHorizontalAlignment(JLabel.CENTER);
+        
+        JButton buttonBoletim = new JButton();
+        buttonBoletim.setBounds(400, 175, 100, 50);
+        buttonBoletim.add(labelBoletim);
+        jFrameInicial.add(buttonBoletim);
+        
+        JLabel labelNotas = new JLabel("Notas");
+        labelNotas.setBounds(20, 20, 100, 100);
+        labelNotas.setHorizontalAlignment(JLabel.CENTER);
+        
+        JButton buttonNotas = new JButton();
+        buttonNotas.setBounds(525, 175, 100, 50);
+        buttonNotas.add(labelNotas);
+        jFrameInicial.add(buttonNotas);
+        
+        JLabel labelInstit = new JLabel("Instituição");
+        labelInstit.setBounds(20, 20, 100, 100);
+        labelInstit.setHorizontalAlignment(JLabel.CENTER);
+        
+        JButton buttonInstit = new JButton();
+        buttonInstit.setBounds(650, 175, 100, 50);
+        buttonInstit.add(labelInstit);
+        jFrameInicial.add(buttonInstit);
+        
+        JLabel labelProf = new JLabel("Professores");
+        labelProf.setBounds(20, 20, 100, 100);
+        labelProf.setHorizontalAlignment(JLabel.CENTER);
+        
+        JButton buttonProf = new JButton();
+        buttonProf.setBounds(775, 175, 100, 50);
+        buttonProf.add(labelProf);
+        jFrameInicial.add(buttonProf);
+        
+        
+        // ainda falta adicionar as funcionalidades!
+        jFrameInicial.setVisible(true);
+        
+    }
+    
 }
